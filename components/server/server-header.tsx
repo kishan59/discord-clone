11ee753eb9a9155f56a8b05afda0611a6be78257
kishan <kishanlamba59@gmail.com>
2026-07-31 20:16:20 +0530

    server delete and leave server functionality implemented

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -59,7 +59,7 @@ export const ServerHeader = ({
                 {isModerator && (
                     <DropdownMenuItem 
                         className="px-3 py-2 text-sm cursor-pointer"
-                        onClick={() => {console.log("heyyyy"); onOpen("createChannel")}}
+                        onClick={() => onOpen("createChannel")}
                     >
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto" />
@@ -69,13 +69,19 @@ export const ServerHeader = ({
                     <DropdownMenuSeparator />
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("deleteServer", { server })}
+                    >
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("leaveServer", { server })}    
+                    >
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
