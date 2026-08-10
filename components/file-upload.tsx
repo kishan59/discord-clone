0847b2dfe62c83bb5c6fc1ba500adaf6c8760eb1
kishan <kishanlamba59@gmail.com>
2026-08-10 22:40:05 +0530

    chat input component, message api, message attachment functionality implemented

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -14,7 +14,7 @@ interface FileUploadProps {
 export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
     const fileType = value?.split(".").pop();
 
-    if (value && fileType != "pdf") {
+    if (value && fileType !== "pdf") {
         return (
             <div className="relative h-20 w-20">
                 <Image 
@@ -33,12 +33,40 @@ export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
             </div>
         )
     }
+
+
+    if (value && fileType === "pdf") {
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+                <a
+                    href={value}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+                >
+                    {value}
+                </a>
+                 <button
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+                    type="button"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+
+            </div>
+        )
+    }
     
     return (
         <UploadDropzone
             endpoint={endpoint}
             onClientUploadComplete={( res ) => {
-                onChange(res?.[0].ufsUrl);
+                const file = res?.[0];
+                const url = file?.ufsUrl;
+                const extension = file?.name.split(".").pop();
+                onChange(`${url}#.${extension}`);
             }}
             onUploadError={(error: Error) => {
                 console.error("Upload error:", error);
